c97dfbd9472b8bc21c649f329b526565773c6df4
Vojtech Sliva <sli0124@vsb.cz>
2024-11-20 12:19:54 +0100

    feat(task1): plotting using ggplot2

--- R/task1.R
+++ R/task1.R
@@ -73,6 +73,7 @@ all_data = all_data %>% select(-id)
 # delete old data frames, just for cleaner environment table
 rm(nvidia_2080, nvidia_3070, amd_6800, amd_7700, new_column_names)
 
+
 # 3. data analysis ####
 # 3.1.
 # add a new column preformance_increase
@@ -151,93 +152,110 @@ summary_amd_7700 = summarize_gpu("amd_7700")
 print(summary_amd_7700)
 
 # 4. data visualization ####
-# 4.1.
+# 4.1. create a histogram for performance increase grouped by GPU
 # create boxplot with outliers for Nvidia 3070 and AMD 7700
-filtered_data <- all_data %>% filter(gpu %in% c("nvidia_3070", "amd_7700"))
-png("output/boxplot_with_outliers.png", width = 800, height = 600)
-par(mfrow = c(1, 1))
-boxplot(filtered_data$performance_increase ~ filtered_data$gpu,
-        names = c("Nvidia 3070", "AMD 7700"),
-        main = "Krabicový graf s odlehlými pozorováními",
-        xlab = "Grafické karty",
-        ylab = "FPS",
-        col="gray",
-        border="black")
-dev.off()
-        
-# 4.2.
-# create boxplot without outliers for Nvidia 3070 and AMD 7700
-filtered_data <- all_data %>% filter(gpu %in% c("nvidia_3070", "amd_7700"), outlier == FALSE)
-png("output/boxplot_without_outliers.png", width = 800, height = 600)
-par(mfrow = c(1, 1))
-boxplot(filtered_data$performance_increase ~ filtered_data$gpu,
-        names = c("Nvidia 3070", "AMD 7700"),
-        main = "Krabicový graf bez odlehlých pozorování",
-        xlab = "Grafické karty",
-        ylab = "FPS",
-        col="gray",
-        border="black")
-dev.off()
+# Filter data for Nvidia 3070 and AMD 7700
+selected_gpus = all_data %>% filter(gpu %in% c("nvidia_3070", "amd_7700"))
 
-# 4.3. 
-# combine QQ plot and histogram for Nvidia 3070
-filtered_data <- all_data %>% filter(gpu == "nvidia_3070", outlier == FALSE)
-png("output/qqplot_histogram_nvidia_3070.png", width = 1200, height = 600)
-par(mfrow = c(1, 2))
-# QQ plot
-qqnorm(filtered_data$performance_increase,
-       main = "QQ plot pro Nvidia 3070",
-       xlab = "Norm. teoretické kvantily",
-       ylab = "Výběrové kvantily")
-qqline(filtered_data$performance_increase, col = "black")
-# histogram
-hist(filtered_data$performance_increase,
-     main = "Histogram pro Nvidia 3070",
-     xlab = "FPS",
-     ylab = "Počet",
-     col = "gray",
-     border = "black")
-par(mfrow = c(1, 1))
-dev.off()
+# Define GPU name mapping
+gpu_name_mapping <- c(
+  "nvidia_3070" = "Nvidia RTX 3070 Ti", 
+  "amd_7700" = "AMD Radeon RX 7700 XT"
+)
 
-# 4.4.
-# combine QQ plot and histogram for AMD 7700
-filtered_data <- all_data %>% filter(gpu == "amd_7700", outlier == FALSE)
-png("output/qqplot_histogram_amd_7700.png", width = 1200, height = 600)
-par(mfrow = c(1, 2))
-# QQ plot
-qqnorm(filtered_data$performance_increase,
-       main = "QQ plot pro AMD 7700",
-       xlab = "Norm. teoretické kvantily",
-       ylab = "Výběrové kvantily")
-qqline(filtered_data$performance_increase, col = "black")
-# histogram
-hist(filtered_data$performance_increase,
-     main = "Histogram pro AMD 7700",
-     xlab = "FPS",
-     ylab = "Počet",
-     col = "gray",
-     border = "black")
-par(mfrow = c(1, 1))
-dev.off()
+# Filter and map GPU names to human-readable names
+selected_gpus <- all_data %>%
+  filter(gpu %in% names(gpu_name_mapping)) %>%
+  mutate(gpu = gpu_name_mapping[gpu])
+
+# Define GPU colors
+gpu_colors <- c("Nvidia RTX 3070 Ti" = "grey", "AMD Radeon RX 7700 XT" = "white")
+
+# Create the boxplot for performance increase grouped by GPU
+boxplot_plot <- ggplot(selected_gpus, aes(x = gpu, y = performance_increase, fill = gpu)) +
+  stat_boxplot(geom = "errorbar", width = 0.2) +  # Add error bars
+  geom_boxplot() +                               # Create the boxplot
+  scale_fill_manual(values = gpu_colors) +       # Set custom colors
+  labs(x = "", y = "Zvýšení výkonu (FPS)") +     # Axis labels
+  theme_bw() +                                   # White background theme
+  theme(legend.position = "none")                # Remove legend
+
+# Save the plot
+ggsave("output/boxplot_with_outliers_task1.png", plot = boxplot_plot, width = 10, height = 6, units = "in", dpi = 300)
+
+# 4.2. create a histogram and qqplot for performance increase grouped by GPU
+# without outliers
+# on left there will be histogram, on right there will be qqplot
+# both sets of plots will will have same x range
+
+# 4.2.1. create a histogram
+# Filter data for Nvidia 3070 and AMD 7700 without outliers
+selected_gpus_no_outliers <- all_data %>% filter(gpu %in% c("nvidia_3070", "amd_7700"), outlier == FALSE)
+
+# apply the name mapping
+selected_gpus_no_outliers <- selected_gpus_no_outliers %>%
+  mutate(gpu = gpu_name_mapping[gpu])
+
+# Create the histogram for performance increase grouped by GPU without outliers
+histogram_plot <- ggplot(selected_gpus_no_outliers, 
+                         aes(x = performance_increase, fill = gpu)) +
+  geom_histogram(color = "black", 
+                 fill = 'lightgrey',
+                 binwidth = 0.25) + 
+  labs(x = "Zvýšení výkonu (FPS)", 
+       y = "Četnost") +
+  theme_bw() + 
+  theme(axis.text = element_text(color = 'black', size = 12)) +
+  facet_wrap(~gpu, , nrow = 4) 
+
+
+# 4.2.2. create a qqplot
+qqplot_plot <- ggplot(selected_gpus_no_outliers, 
+                      aes(sample = performance_increase)) +
+  stat_qq() + 
+  stat_qq_line() +
+  labs(x = "Normované teoretické kvantily", 
+       y = "Výběrové kvantily") +
+  theme_bw() +
+  theme(axis.text = element_text(color = 'black', size = 12)) +
+  facet_wrap(~gpu, , nrow = 4,
+             scales = "free")
+
+# 4.2.3. combine the plots
+combined_plot <- ggarrange(histogram_plot, qqplot_plot,
+                           nrow = 1)
+ggarrange(histogram_plot, qqplot_plot, 
+          nrow = 2,
+          heights = c(2,4))
+          
+
+# save the plot
+ggsave("output/histogram_qqplot_no_outliers_task1.png", plot = combined_plot, width = 10, height = 6, units = "in", dpi = 300)
+
+# clear the environment
+rm(selected_gpus, selected_gpus_no_outliers, gpu_name_mapping, gpu_colors, boxplot_plot, histogram_plot, qqplot_plot, combined_plot)
 
 # 5. calculate sigma interval ####
+
 calculate_sigma_interval = function(data, column_name, sigma) {
-  mean_value = mean(data[[column_name]], na.rm = TRUE)
-  sd_value = sd(data[[column_name]], na.rm = TRUE)
+  prumer = mean(data[[column_name]])
+  smerodatna_odchylka = sd(data[[column_name]])
+  rozptyl = smerodatna_odchylka^2
   
-  lower_bound = mean_value - sigma * sd_value
-  upper_bound = mean_value + sigma * sd_value
+  vrchni_hranice = prumer + sigma * smerodatna_odchylka
+  dolni_hranice = prumer - sigma * smerodatna_odchylka
   
-  return(c(lower_bound, upper_bound))
+  return(c(dolni_hranice, vrchni_hranice))
 }
 
 # calculate 2-sigma interval for Nvidia 3070
 nvidia_3070 = all_data %>% filter(gpu == "nvidia_3070", outlier == FALSE)
 sigma_interval_2_3070 = calculate_sigma_interval(nvidia_3070, "performance_increase", 2)
+print(sigma_interval_2_3070)
 
 # calculate 2-sigma interval for AMD 7700
 amd_7700 = all_data %>% filter(gpu == "amd_7700", outlier == FALSE)
 sigma_interval_2_7700 = calculate_sigma_interval(amd_7700, "performance_increase", 2)
+print(sigma_interval_2_7700)
 
 
